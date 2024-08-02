6061b4cfa06a4313f1b49e78f4ab6061e16d4b28
Jeremy Long <jeremy.long@gmail.com>
2024-08-02 06:07:06 -0400

    chore: cleanup

--- jakarta-test/src/main/java/org/owasp/encoder/testing/jakarta_test/controller/ItemController.java
+++ jakarta-test/src/main/java/org/owasp/encoder/testing/jakarta_test/controller/ItemController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.owasp.encoder.testing.jakarta_test.controller;
 
 import org.owasp.encoder.testing.jakarta_test.service.ItemService;
